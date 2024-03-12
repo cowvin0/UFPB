5bc156dcd030b2fa97160409437a49b9d0a83a82
cowvin0 <gabrielpereira00PereiraGabriel@outlook.com>
2024-03-12 17:34:39 -0300

    .

--- python/py_nump.py
+++ python/py_nump.py
@@ -1,15 +1,12 @@
+from scipy.integrate import quad
 import numpy as np
-import functools
 
+def integrar(func):
+    def integra(x, lamb):
+        resultado = quad(lambda a: func(a, lamb), 0, 100)
+        return abs(1 - resultado[0]) < 1e-2 
+    return integra
 
-x = np.arange(1, 11, 1)
-
-print(x[3:9])
-print(x[-8:-2])
-print(x[-8:9])
-print([x for x in x if x > 2 and x < 9])
-print(list(filter(lambda i: i > 2 and i < 9, x)))
-print(x[(x >= 3) & (x < 9)])
-print(x[3:-2])
-
-x.reshape(2, 5, order="C")
+@integrar
+def pdf_exp(x, lamb):
+    return lamb * np.exp(- lamb * x)
