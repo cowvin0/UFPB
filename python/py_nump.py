ae487450e63417f2a9443a1d5fc9b9d3de5dc7cc
cowvin0 <gabrielpereira00PereiraGabriel@outlook.com>
2024-03-24 07:18:17 -0300

    .

--- python/py_nump.py
+++ python/py_nump.py
@@ -1,12 +1,27 @@
 from scipy.integrate import quad
+from scipy.optimize import minimize
 import numpy as np
 
-def integrar(func):
-    def integra(x, lamb):
-        resultado = quad(lambda a: func(a, lamb), 0, 100)
-        return abs(1 - resultado[0]) < 1e-2 
-    return integra
+def integrar(error):
+    def integra1(func):
+        def integra2(*args):
+            resultado = quad(lambda a: func(a, args[1]), 0, 100)
+            return abs(1 - resultado[0]) < error
+        return integra2
+    return integra1
 
-@integrar
+@integrar(error=1e-15)
 def pdf_exp(x, lamb):
     return lamb * np.exp(- lamb * x)
+
+# def minimiza(x0):
+#     def minimiza1(func):
+#         def minimiza2(*args):
+#             def objective(parameters):
+
+
+
+# def maxinho_exp(x, lamb):
+#     n = len(x)
+#     return n * np.log(lamb) - lamb * sum(x)
+
