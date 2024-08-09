b59e689464941f9e40e62bc860de8f6fe23341f9
cowvin0 <gabrielpereira00PereiraGabriel@outlook.com>
2024-08-09 10:31:52 -0300

    .

--- sobrevivencia/exercicios1.r
+++ sobrevivencia/exercicios1.r
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(survival)
 library(ggplot2)
 library(ggfortify)
@@ -129,3 +130,74 @@ funcao_risco_weibull <- \(t, alpha, gamma) {
 taxafalha_30 <- funcao_risco_weibull(30, 100, 2)
 taxafalha_45 <- funcao_risco_weibull(45, 100, 2)
 taxafalha_60 <- funcao_risco_weibull(60, 100, 2)
+
+# Questão 2
+
+
+
+# Questão 3
+
+tempos_cap3 <- c(
+	0.19, 0.78, 0.96, 1.31, 2.78,
+	3.16, 4.67, 4.85, 6.50, 7.35,
+	8.27, 12.07, 32.52, 33.91, 36.71, rep(37, 10)
+)
+
+cens_cap3 <- c(
+	rep(1, 15), rep(0, 10)
+)
+
+ajust1_exp <- survreg(
+	Surv(tempos_cap3, cens_cap3) ~ 1, dist = "exponential"
+)
+lambda <- exp(ajust1_exp$coefficients[1])
+
+ajust1_weibull <- survreg(
+	Surv(tempos_cap3, cens_cap3) ~ 1, dist = "weibull"
+)
+alpha <- exp(ajust1_weibull$coefficients[1])
+gama <- 1 / ajust1_weibull$scale
+
+ajust1_lognormal <- survreg(
+	Surv(tempos_cap3, cens_cap3) ~ 1, dist = "lognormal"
+)
+
+ekm_cap3_quest3 <- survfit(Surv(tempos_cap3, cens_cap3) ~ 1)
+time <- ekm_cap3_quest3$time
+ste <- exp(-time/lambda)
+stw <- exp(-(time/alpha)^gama)
+stln <- pnorm((-log(time) + ajust1_lognormal$coefficients)/ajust1_lognormal$scale)
+
+tabela_ajustados <- tibble(
+	Tempos = time,
+	`Kaplan-Meier` = ekm_cap3_quest3$surv,
+	Exponencial = ste,
+	Weibull = stw,
+	`Log-normal` = stln
+)
+
+tabela_ajustados |>
+	ggplot(aes(x = `Kaplan-Meier`, y = Exponencial)) +
+	geom_point() +
+	geom_abline()
+
+tabela_ajustados |>
+	ggplot(aes(x = `Kaplan-Meier`, y = Weibull)) +
+	geom_point() +
+	geom_abline()
+
+tabela_ajustados |>
+	ggplot(aes(x = `Kaplan-Meier`, y = `Log-normal`)) +
+	geom_point() +
+	geom_abline()
+
+# tabela_ajustados |>
+# 	pivot_longer(
+# 		cols = -c(Tempos, `Kaplan-Meier`),
+# 		names_to = "Distribuição",
+# 		values_to = "S(t)"
+# 	) |>
+# 	ggplot(aes(x = `Kaplan-Meier`, y = "S(t)")) +
+# 	geom_point() +
+# 	geom_abline() +
+# 	facet_wrap(vars(Distribuição), scales = "free")
